b58acf6c8a8b40136087f655372ca3e53269cd5f
slimved <87672887+slimved@users.noreply.github.com>
2021-09-19 00:10:48 +0530

    Update intersection-of-two-linked-lists.cpp

--- intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists.cpp
@@ -29,3 +29,12 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
         
     return p1;
 }
+
+ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB) {
+    ListNode *cur1 = headA, *cur2 = headB;
+    while(cur1 != cur2){
+        cur1 = cur1?cur1->next:headB;
+        cur2 = cur2?cur2->next:headA;
+    }
+    return cur1;
+}
